e1d2fdb46026fbdb6d392be812a1587fa375161d
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-03-13 22:31:12 +0300

    It will add projection function

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 function milkfunction() {
-  shed1 =(document.getElementById("firstshed").value);
-  shed2 =(document.getElementById("secondshed").value);
-  shed3 =(document.getElementById("thirdshed").value);
-  shed4 =(document.getElementById("forthshed").value);
+  var shed1 =(document.getElementById("firstshed").value);
+  var shed2 =(document.getElementById("secondshed").value);
+  var shed3 =(document.getElementById("thirdshed").value);
+  var shed4 =(document.getElementById("forthshed").value);
 
   if(shed1.length === 0 || shed2.length === 0 || shed3.length === 0 || shed4.length === 0) {
     //check whether the user has typed something
@@ -28,3 +28,19 @@ function milkfunction() {
 function reset() {
     location.reload();
   }
+  function projection(selling_price,time){
+   var production=(document.getElementById("total").value);
+   if(production.length===0){
+     alert("Empty fields not allowed");
+   }else{
+     production =parseFloat(production);
+    var weekly=45*production*7;
+    var monthly=45*production*30;
+    var yearly=45*production*365;
+    document.getElementById("hidden2").style.display = 'block';
+    document.getElementById("weekly").innerHTML=weekly;
+    document.getElementById("monthly").innerHTML=monthly;
+    document.getElementById("yearly").innerHTML=yearly;
+   }
+  }
+  

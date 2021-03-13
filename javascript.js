0b9771313f30fa3c61157574de741fd3ff84361b
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-03-13 16:55:13 +0300

    It will add a pop up warning when user enter empty field

--- javascript.js
+++ javascript.js
@@ -3,6 +3,14 @@ function milkfunction() {
   shed2 =(document.getElementById("secondshed").value);
   shed3 =(document.getElementById("thirdshed").value);
   shed4 =(document.getElementById("forthshed").value);
+
+  if(shed1.length === 0 || shed2.length === 0 || shed3.length === 0 || shed4.length === 0) {
+    //check whether the user has typed something
+
+      alert("Fill all the filds!");
+    }
+  
+  else {
       //convert the strings to numbers
       shed1 = parseFloat(shed1);
       shed2 = parseFloat(shed2);
@@ -15,6 +23,7 @@ function milkfunction() {
       document.getElementById("shed3").innerHTML=shed3;
       document.getElementById("shed4").innerHTML=shed4;
       document.getElementById("totalmilk").innerHTML = totalmilk;
+  }
 }
 function reset() {
     location.reload();
